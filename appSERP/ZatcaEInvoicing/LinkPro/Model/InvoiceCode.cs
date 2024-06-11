152888ac33d27876ee5186cd0b37925ac5e10855
bh <bhbmhh@gmail.com>
2024-06-11 13:30:15 +0300

    push test

--- appSERP/ZatcaEInvoicing/LinkPro/Model/InvoiceCode.cs
+++ appSERP/ZatcaEInvoicing/LinkPro/Model/InvoiceCode.cs
@@ -8,6 +8,7 @@ namespace appSERP.ZatcaEInvoicing.LinkPro.Model
     public class InvoiceCode
     {
         //invoice, credit, debit
+        // bh
         static string InvoiceCode_invoice = "invoice";
         static string InvoiceCode_credit = "credit";
         static string InvoiceCode_debit = "debit";
